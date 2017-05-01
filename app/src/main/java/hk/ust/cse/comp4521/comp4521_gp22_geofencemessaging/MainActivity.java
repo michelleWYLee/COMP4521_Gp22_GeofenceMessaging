f431f174a4c14a23abb607daab8fe0c950f47b9d
germw <cpwongak@connect.ust.hk>
2017-05-01 11:13:45 +0800

    bug fixed

--- app/src/main/java/hk/ust/cse/comp4521/comp4521_gp22_geofencemessaging/MainActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/comp4521_gp22_geofencemessaging/MainActivity.java
@@ -22,33 +22,11 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-//check location and gps permission
-
-
-    // Create an instance of GoogleAPIClient.
-    protected GoogleApiClient mGoogleApiClient;
-
-    private static final int PERMISSION_LOCATION_REQUEST_CODE = 0;
-    private String TAG = "Geofence Message";
-
-    private Location currentLocation;
-    // Request code to use when launching the resolution activity
-    private static final int REQUEST_RESOLVE_ERROR = 1001;
-    // Unique tag for the error dialog fragment
-    private static final String DIALOG_ERROR = "dialog_error";
-    // Bool to track whether the app is already resolving an error
-    private boolean mResolvingError = false;
-    private LocationRequest locationRequest;
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Build the Google API client so that connections can be established
-        buildGoogleApiClient();
-        configLocationRequest();
 
         setContentView(R.layout.activity_main);
 
@@ -56,42 +34,28 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
 
-        TabStatePagerAdapter adapter = new TabStatePagerAdapter(getSupportFragmentManager(),currentLocation);
+        TabPagerAdapter adapter = new TabPagerAdapter(getSupportFragmentManager(),MainActivity.this);
 
         viewPager.setAdapter(adapter);
         viewPager.setCurrentItem(1);
 
         // TabLayout mixed with ViewPager.
-
         tabLayout.setupWithViewPager(viewPager);
         tabLayout.setTabMode(TabLayout.MODE_FIXED);
+
+        //Iterate over all tabs and set custom view
+//        for(int i=0; i<tabLayout.getTabCount();i++){
+//            TabLayout.Tab tab = tabLayout.getTabAt(i);
+//            tab.setCustomView(adapter.getTab)
+//        }
     }
 
-    /**
-     * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
-     */
-    protected synchronized void buildGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
 
-    protected void configLocationRequest(){
-        locationRequest = new LocationRequest();
-        //set get location time to 1s =1000ms
-        locationRequest.setInterval(1000);
-        locationRequest.setFastestInterval(1000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_LOW_POWER);
-
-    }
 
     @Override
     protected void onStart() {
 
         super.onStart();
-        mGoogleApiClient.connect();
     }
 
     @Override
@@ -114,9 +78,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     protected void onStop() {
-        if (mGoogleApiClient.isConnected()) {
-            mGoogleApiClient.disconnect();
-        }
+
         super.onStop();
     }
 
@@ -126,106 +88,18 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         super.onDestroy();
     }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        // Provides a simple way of getting a device's location and is well suited for
-        // applications that do not require a fine-grained location and that do not need location
-        // updates. Gets the best and most recent location currently available, which may be null
-        // in rare cases when a location is not available.
-
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(
-                    this,
-                    new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSION_LOCATION_REQUEST_CODE);
-
-            return;
-        }
-        currentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-
-        if (currentLocation != null) {
-            /*String message = "Last Location is: " +
-                    "  Latitude = " + String.valueOf(currentLocation.getLatitude()) +
-                    "  Longitude = " + String.valueOf(currentLocation.getLongitude());
-            Log.i(TAG, message);*/
-
-            Toast.makeText(this, "location connected", Toast.LENGTH_LONG).show();
-
-
-        } else {
-            Toast.makeText(this, "no service available,please try again", Toast.LENGTH_LONG).show();
-        }
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.i(TAG, "Connection suspended");
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult result) {
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
-
-        if (mResolvingError) {
-            // Already attempting to resolve an error.
-            return;
-        } else if (result.hasResolution()) {
-            try {
-                mResolvingError = true;
-                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
-            } catch (IntentSender.SendIntentException e) {
-                // There was an error with the resolution intent. Try again.
-                mGoogleApiClient.connect();
-            }
-        } else {
-            // Show dialog using GooglePlayServicesUtil.getErrorDialog()
-            showErrorDialog(result.getErrorCode());
-            mResolvingError = true;
-        }
-
-    }
-
-
-
-
-
-    // The rest of this code is all about building the error dialog
-
-    /* Creates a dialog for an error message */
-    private void showErrorDialog(int errorCode) {
-        // Create a fragment for the error dialog
-        ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
-        // Pass the error that should be displayed
-        Bundle args = new Bundle();
-        args.putInt(DIALOG_ERROR, errorCode);
-        dialogFragment.setArguments(args);
-        dialogFragment.show(getFragmentManager(), "errordialog");
-    }
-
-    /* Called from ErrorDialogFragment when the dialog is dismissed. */
-    public void onDialogDismissed() {
-        mResolvingError = false;
-    }
-
-    /* A fragment to display an error dialog */
-    public static class ErrorDialogFragment extends DialogFragment {
-        public ErrorDialogFragment() { }
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Get the error code and retrieve the appropriate dialog
-            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
-            return GooglePlayServicesUtil.getErrorDialog(errorCode,
-                    this.getActivity(), REQUEST_RESOLVE_ERROR);
-        }
-
-        @Override
-        public void onDismiss(DialogInterface dialog) {
-            ((MainActivity)getActivity()).onDialogDismissed();
-        }
-    }
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode,
+//                                           String permissions[], int[] grantResults) {
+//        if (requestCode == MapFragment.MY_PERMISSIONS_REQUEST_LOCATION){
+//            MapFragment mapFragment = (MapFragment) TabPagerAdapter.fragments[0];
+//            if (mapFragment != null) {
+//                mapFragment.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//            }
+//        }
+//        else {
+//            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        }
+//    }
 
 }
