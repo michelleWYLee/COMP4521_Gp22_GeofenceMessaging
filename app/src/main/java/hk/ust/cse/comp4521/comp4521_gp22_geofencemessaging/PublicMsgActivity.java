b590dce7d9424aedf77a15c31a014df006fbd23b
germw <cpwongak@connect.ust.hk>
2017-05-18 23:34:49 +0800

    with appbar in progress

--- app/src/main/java/hk/ust/cse/comp4521/comp4521_gp22_geofencemessaging/PublicMsgActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/comp4521_gp22_geofencemessaging/PublicMsgActivity.java
@@ -18,6 +18,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
@@ -77,6 +78,9 @@ public class PublicMsgActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_public_msg);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         buildGoogleApiClient();
 
