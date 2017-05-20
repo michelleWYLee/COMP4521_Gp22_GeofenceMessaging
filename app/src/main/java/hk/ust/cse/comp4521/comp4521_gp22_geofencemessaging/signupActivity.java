09cff69ae403a19d39aa10833ddd7c11c67fe743
germw <cpwongak@connect.ust.hk>
2017-05-20 21:10:29 +0800

    signup with username

--- app/src/main/java/hk/ust/cse/comp4521/comp4521_gp22_geofencemessaging/signupActivity.java
+++ app/src/main/java/hk/ust/cse/comp4521/comp4521_gp22_geofencemessaging/signupActivity.java
@@ -15,15 +15,20 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 //TO-DO ADD USER NAME TO ACCOUNT WHEN REGISTER
 
 public class signupActivity extends AppCompatActivity {
 
-    private EditText inputEmail, inputPassword;
+    private EditText inputEmail, inputPassword,inputUserName;
     private Button btnSignIn, btnSignUp, btnResetPassword;
     private ProgressBar progressBar;
     private FirebaseAuth auth;
+    private DatabaseReference mDatabase;
+    String getUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +37,13 @@ public class signupActivity extends AppCompatActivity {
 
         //Get Firebase auth instance
         auth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         btnSignIn = (Button) findViewById(R.id.sign_in_button);
         btnSignUp = (Button) findViewById(R.id.sign_up_button);
         inputEmail = (EditText) findViewById(R.id.email);
         inputPassword = (EditText) findViewById(R.id.password);
+        inputUserName =(EditText) findViewById(R.id.username);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         btnResetPassword = (Button) findViewById(R.id.btn_reset_password);
 
@@ -60,6 +67,7 @@ public class signupActivity extends AppCompatActivity {
 
                 String email = inputEmail.getText().toString().trim();
                 String password = inputPassword.getText().toString().trim();
+                final String username = inputUserName.getText().toString().trim();
 
                 if (TextUtils.isEmpty(email)) {
                     Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
@@ -71,10 +79,16 @@ public class signupActivity extends AppCompatActivity {
                     return;
                 }
 
+                if (TextUtils.isEmpty(username)) {
+                    Toast.makeText(getApplicationContext(), "Enter Username!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 if (password.length() < 6) {
                     Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
                     return;
                 }
+
 
                 progressBar.setVisibility(View.VISIBLE);
                 //create user
@@ -88,17 +102,39 @@ public class signupActivity extends AppCompatActivity {
                                 // the auth state listener will be notified and logic to handle the
                                 // signed in user can be handled in the listener.
                                 if (!task.isSuccessful()) {
+
                                     Toast.makeText(signupActivity.this, "Authentication failed." + task.getException(),
                                             Toast.LENGTH_SHORT).show();
                                 } else {
-                                    startActivity(new Intent(signupActivity.this, PublicMsgActivity.class));
-                                    finish();
+                                    getUserName = username;
+                                    onAuthSuccess(task.getResult().getUser(),getUserName);
+
                                 }
                             }
                         });
 
             }
         });
+    }
+
+    private void onAuthSuccess(FirebaseUser user, String username){
+
+        mDatabase.child("users").child(user.getUid()).child("name").setValue(username);
+
+        //if successfully registered
+        startActivity(new Intent(signupActivity.this, PublicMsgActivity.class));
+        finish();
+
+
+    }
+
+    private String usernameFromEmail(String email) {
+        if (email.contains("@")) {
+
+            return email.split("@")[0];
+        } else {
+            return email;
+        }
     }
 
     @Override
